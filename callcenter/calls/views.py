884d6a6cfe0e6b83dae31aa40e3dea9596cf01dd
Marcelo dos Santos Moraes Junior <marcelo.moraes@saeinfnt009.saedigital.local>
2018-04-26 21:20:41 -0300

    Add post method

--- callcenter/calls/views.py
+++ callcenter/calls/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Call
+from .serializers import CallSerializer
+
+
+class CallViewSet(APIView):
+    """
+    CallViewSet
+    """
+    def post(self, request, format=None):
+        """
+        Create a new register
+        :param request:
+        :param format:
+        :return:
+        """
+        call_serializer = CallSerializer(data=request.data)
+        if call_serializer.is_valid():
+            call_serializer.save()
+            return Response(call_serializer.data,
+                            status=status.HTTP_201_CREATED)
+        return Response(call_serializer.errors,
+                        status=status.HTTP_400_BAD_REQUEST)
