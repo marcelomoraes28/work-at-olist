a9958a17699765c05329207b1115cff1f8b54e59
Marcelo dos Santos Moraes Junior <marcelo.moraes@saeinfnt009.saedigital.local>
2018-05-12 20:04:00 -0300

    Create bill route

--- call_project/calls/urls.py
+++ call_project/calls/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^calls$', views.CallViewSet.as_view(), name='calls'),
+    url(r'^bill/(?P<source>[0-9]+)/(?P<month>[0-9]+)/(?P<year>[0-9]+)/$',
+        views.BillViewSet.as_view(), name='bill'),
 ]
--- call_project/calls/views.py
+++ call_project/calls/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -49,10 +51,31 @@ class CallViewSet(APIView):
         return Response(call_serializer.errors,
                         status=status.HTTP_400_BAD_REQUEST)
 
-    def get(self, request, format=None):
+
+class BillViewSet(APIView):
+    """
+    This endpoint presents Bills
+    """
+
+    def _get_bill_objects(self, source, month, year):
+        bills = Bill.objects.filter(source=source,
+                                    call_start_date__year__gte=year,
+                                    call_start_date__month__gte=month)
+        if not bills:
+            raise Http404
+        return bills
+
+    def get(self, request, source, month, year):
         """
-        List all bills
+        Parameters
+        ----------
+        source/month/year
+
+        ----------
+        Example to get a bill:
+        ----------
+        calls/bill/10/2018
         """
-        bill = Bill.objects.all()
+        bill = self._get_bill_objects(source, month, year)
         serializer = BillSerializer(bill, many=True)
         return Response(serializer.data)
