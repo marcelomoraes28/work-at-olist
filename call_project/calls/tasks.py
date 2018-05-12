20335fe3aeb5fd6b0df02e48c3aa31210d66d6d3
Marcelo dos Santos Moraes Junior <marcelo.moraes@saeinfnt009.saedigital.local>
2018-05-12 18:58:08 -0300

    Refactoring and add retry

--- call_project/calls/tasks.py
+++ call_project/calls/tasks.py
@@ -3,25 +3,28 @@ from call_project.celery import app
 from .cost import CalculateCost
 from .models import Cost, STATUS, Bill, Call
 
-@app.task(name='generate_bill', default_retry_delay=30)
-def generate_bill(call_id):
+
+@app.task(name='generate_bill', bind=True)
+def generate_bill(self, call_id):
     """
     Calculate and generate a Bill
     :param call_id:
     :return:
     """
-    calls = Call.objects.filter(call_id=call_id).order_by('call_type')
-    cost = Cost.objects.filter(status=STATUS[0][0]).last()
-    if not cost:
-        raise ValueError("Attention, it is necessary to register a cost")
-    if not calls:
-        raise ValueError("That call doesn't not exist")
-    if calls.count() == 1:
-        # TODO: Retry
-        pass
+    # TODO: send excptions to Sentry, NewRelic, Graylog
     try:
-        calculate_cost = CalculateCost(cost.cost_per_minute, cost.connection_cost,
-                                       str(cost.initial_period), str(cost.end_period))
+        calls = Call.objects.filter(call_id=call_id).order_by('call_type')
+        cost = Cost.objects.get(status=STATUS[0][0])
+        if not calls:
+            raise ValueError("Call doesn't not exist.")
+
+        if calls.count() == 1:
+            raise self.retry(countdown=15)
+
+        calculate_cost = CalculateCost(cost.cost_per_minute,
+                                       cost.connection_cost,
+                                       str(cost.initial_period),
+                                       str(cost.end_period))
         calculate_bill = calculate_cost.calculate_cost_per_period(
             str(calls[0].timestamp)[0:19], str(calls[1].timestamp)[0:19])
         Bill.objects.create(destination=calls[1].destination,
@@ -30,5 +33,8 @@ def generate_bill(call_id):
                             call_start_time=str(calls[0].timestamp)[11:19],
                             call_price=float(calculate_bill['cost']),
                             duration=calculate_bill['duration'])
-    except Exception as e:
-        raise e
+
+    except Cost.DoesNotExist as exc:
+        raise self.retry(exc=exc, countdown=15)
+
+
