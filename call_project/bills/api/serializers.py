a0a09bd7b6c432313362604c61190212a3db51a3
Marcelo dos Santos Moraes Junior <marcelo.moraes@saeinfnt009.saedigital.local>
2018-05-19 19:14:29 -0300

    Changed BillSerializer extend to ModelSerializer

--- call_project/bills/api/serializers.py
+++ call_project/bills/api/serializers.py
@@ -1,23 +1,11 @@
 from rest_framework import serializers
 
-from call_project.calls.models import Bill
+from bills.models import Bill
 
 
-class BillSerializer(serializers.Serializer):
-
-    call_id = serializers.IntegerField(required=True)
-    timestamp = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S",
-                                          required=True)
-
-    def create(self, validated_data):
-        try:
-            bill_exist = Bill.objects.get(call_id=validated_data['call_id'])
-            return bill_exist
-        except Bill.DoesNotExist:
-            created = Bill.objects.create(**validated_data)
-            return created
+class BillSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Bill
-        fields = ('destination', 'timestamp',
+        fields = ('destination', 'call_start_date', 'call_start_time',
                   'call_id', 'call_price')
