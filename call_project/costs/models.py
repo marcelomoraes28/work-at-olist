4f5a270682045b155fb0c8e2c279374859ace8e9
Marcelo dos Santos Moraes Junior <marcelo.moraes@saeinfnt009.saedigital.local>
2018-05-19 15:39:28 -0300

    Renamed verbose names to english language

--- call_project/costs/models.py
+++ call_project/costs/models.py
@@ -1,33 +1,38 @@
 from django.db import models
 
+STATUS = (
+    (1, "Ativo"),
+    (2, "Inativo")
+)
 
 class Cost(models.Model):
     """
     Configuration cost per calls
     """
+
     created_at = models.DateField(
-        verbose_name='Criado em',
+        verbose_name='Created at',
         auto_now_add=True
     )
 
     connection_cost = models.FloatField(
-        verbose_name="Valor por ligação",
+        verbose_name="Price of call",
     )
 
     cost_per_minute = models.FloatField(
-        verbose_name='Valor por minuto',
+        verbose_name='Price per minute',
     )
 
     initial_period = models.TimeField(
-        verbose_name='Início do período comercial'
+        verbose_name='Start of trading period'
     )
 
     end_period = models.TimeField(
-        verbose_name="Final do período comercial"
+        verbose_name="End of trading period"
     )
-    status = models.IntegerField(choices=STATUS, verbose_name="Situação")
+    status = models.IntegerField(choices=STATUS, verbose_name="Status")
 
     # Metadata
     class Meta:
         ordering = ["-id"]
-        verbose_name = 'Configuração dos custos'
+        verbose_name = 'Cost setting'
