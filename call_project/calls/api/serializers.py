4588b2e773cefb651a6dadf342b5e28443fa3036
Marcelo dos Santos Moraes Junior <marcelo.moraes@saeinfnt009.saedigital.local>
2018-05-19 15:31:18 -0300

    CallSerializer extends ModelSerializer

--- call_project/calls/api/serializers.py
+++ call_project/calls/api/serializers.py
@@ -1,55 +1,22 @@
 from rest_framework import serializers
-
-from .validators.custom_validator import RequiredIf
-from .models import Call, Bill, TYPES
-from .tasks import generate_bill
-
-
-class BillSerializer(serializers.Serializer):
-
-    call_id = serializers.IntegerField(required=True)
-    timestamp = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S",
-                                          required=True)
-
-    def create(self, validated_data):
-        try:
-            bill_exist = Bill.objects.get(call_id=validated_data['call_id'])
-            return bill_exist
-        except Bill.DoesNotExist:
-            created = Bill.objects.create(**validated_data)
-            return created
-
-    class Meta:
-        model = Bill
-        fields = ('destination', 'timestamp',
-                  'call_id', 'call_price')
+from validators.custom_validator import RequiredIf
+from calls.models import Call, TYPES
+from bills.tasks import generate_bill
 
 
-class CallSerializer(serializers.Serializer):
+class CallSerializer(serializers.ModelSerializer):
     """
     Call Serializer class
     """
-    call_id = BillSerializer()
-    destination = serializers.CharField(min_length=10,
-                                        max_length=11,
-                                        required=False
-                                        )
-    type = serializers.ChoiceField(choices=TYPES, required=True)
-    source = serializers.CharField(min_length=10,
-                                   max_length=11,
-                                   required=False
-                                   )
-    timestamp = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S",
-                                          required=True)
 
     def create(self, validated_data):
         created = Call.objects.create(**validated_data)
         if created.type == TYPES[1][0]:
-            last = Call.objects.filter(call_id=created.call_id.call_id).last()
+            last = Call.objects.filter(call_id=created.call_id).last()
             created.source = last.source
             created.destination = last.destination
             created.save()
-            generate_bill.delay(created.call_id.call_id)
+            generate_bill.delay(created.call_id)
         return created
 
     def validate(self, data):
@@ -58,7 +25,7 @@ class CallSerializer(serializers.Serializer):
         :param data:
         :return:
         """
-        call_exist = Call.objects.filter(call_id=data['call_id'].get('call_id'),
+        call_exist = Call.objects.filter(call_id=data['call_id'],
                                          type=TYPES[1][0]).first()
         if call_exist:
             raise serializers.ValidationError(
@@ -70,7 +37,8 @@ class CallSerializer(serializers.Serializer):
                 raise serializers.ValidationError(
                     'Warning, this call has already been terminated.')
         else:
-            call_finished = Call.objects.filter(call_id=data['call_id'].get('call_id')).first()
+            call_finished = Call.objects.filter(
+                call_id=data['call_id']).first()
             if call_finished and call_finished.type == TYPES[1][0]:
                 raise serializers.ValidationError(
                     "Hey, This call has already been closed.")
@@ -84,8 +52,7 @@ class CallSerializer(serializers.Serializer):
 
     class Meta:
         model = Call
-        fields = (
-            'type', 'call_id', 'source', 'destination', 'timestamp')
+        fields = ('type', 'call_id', 'source', 'destination', 'timestamp')
         validators = [RequiredIf(fields=('call_id',),
                                  condition=('type', 2)),
                       RequiredIf(fields=('source', 'destination'),
