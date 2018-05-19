63ea5f697956d582d2a25074695a6672983f3d49
Marcelo dos Santos Moraes Junior <marcelo.moraes@saeinfnt009.saedigital.local>
2018-05-19 19:14:55 -0300

    Register bill

--- call_project/bills/admin.py
+++ call_project/bills/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Bill
 
-# Register your models here.
+
+class BillAdmin(admin.ModelAdmin):
+    list_display = (
+        'destination', 'call_id', 'call_start_date', 'call_start_time',
+        'call_price')
+    list_filter = ('destination', 'call_id')
+
+
+admin.site.register(Bill, BillAdmin)
